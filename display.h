cd24095748164d24ec3ec8eaa9b5c15da703c9bc
BenSwolo <35034301+BenSwolo@users.noreply.github.com>
2018-01-02 14:55:17 +0000

    Update display.h

--- display.h
+++ display.h
@@ -9,9 +9,6 @@ void	my_putpixel(unsigned int x, unsigned int y,
 void	my_putcircle(int x0, int y0, int radius,
 		     t_my_pixel new_pixel, t_my_pixel *pixel_array,
 		     int width, int height);
-void	my_putfullcircle(int x0, int y0, int radius,
-			 t_my_pixel new_pixel, t_my_pixel *pixel_array,
-			 int width, int height);
 void	my_putline(int x0, int y0, int x1, int y1,
 		   t_my_pixel new_pixel, t_my_pixel *pixel_array,
 		   int width, int height);
